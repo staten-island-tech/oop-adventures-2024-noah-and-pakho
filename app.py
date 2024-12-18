b74dcd96d4aca64947461018c0da18643ef34a27
pacot2 <pacot2@nycstudents.net>
2024-12-18 08:39:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import requests
 from tkinter import filedialog, Tk, Menu, Listbox, Button, Frame, PhotoImage, END
 import pygame
 import os
 
+app = Tk()
+app.title('Test1')
+app.geometry("800x800")
