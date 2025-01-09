77e031b5354441cac74bdc4f715e0164316cf8a3
pacot2 <pacot2@nycstudents.net>
2025-01-09 09:11:39 -0500

    ughiasdhg;alskdhgO;DLIGKHA;LSDIGH;LWEIGHA;EDLSIKGHA;OWEILGHK;ALSDIGHK;WEOILADKGHAWE;OIGHA;OEIWHG;AWO;EIGHAWEO;UIGHA;WEGIO;HWEDGIO;H/DVFK.AEGUI;RGDSLWEJO/WEJO/WEJO/G;l

--- testing 2.py
+++ testing 2.py
@@ -40,7 +40,7 @@ class Enemy: # a class for enemies you fight against
 
         if selectedEnemyMove:
             print(f"{self.name} uses {selectedEnemyMove} on {targetHero.name}!")
-            damage = random.randint(2, 7) * (self.strength / 5)
+            damage = random.randint(2, 7) * (self.strength / 4)
             targetHero.currentHealth -= damage
             print(f"{self.name} dealt {damage} damage to {targetHero.name}!")
 
@@ -90,7 +90,7 @@ class Game:
                     self.selectedHero = hero 
                     return 
             input("Invalid position selected. Press enter to retry. ") 
-            os.system("cls") 
+            os.system("cls")
   
     def displayEnemyParty(self):
         os.system("cls")
@@ -165,6 +165,7 @@ class Game:
                     damage = selectedMove.damage + (self.selectedHero.level * 2)
                     self.selectedEnemy.currentHealth -= damage
                     print(f"{self.selectedHero.name} dealt {damage} damage to {self.selectedEnemy.name}!")
+                    input("Press Enter to continue. ")
 
                     if self.selectedEnemy.currentHealth <= 0:
                         self.selectedEnemy.currentHealth = 0
@@ -200,16 +201,17 @@ dropKick = Attack(damage = 13, energyCost = 13, name = "Drop Kick")
 whack = Attack(damage = 5, energyCost = 1, name = "Whack")
 cook = Attack(damage = 1, energyCost = 15, name = "Cook")
 study = Attack(damage = 0, energyCost = 10, name = "Study")
+testmove = Attack(damage = 999, energyCost = 0, name = "Test")
 
 # characters/heroes
 Jade = Hero("Jade", 110, 110, 0, 100, 10, 1, [("Shoulder Bash", "A", shoulderBash), ("Uppercut", "B", uppercut), ("Toss", "C", toss)], "A")
 Kelsey = Hero("Kelsey", 80, 80, 0, 90, 8, 1, [("Jab", "A", jab), ("Drop Kick", "B", dropKick)], "B")
 Cashmere = Hero("Cashmere", 100, 100, 0, 90, 7, 1, [("Cook", "A", cook), ("Whack", "B", whack)], "C")
-Ceres = Hero("Ceres", 90, 90, 0, 50, 8, 1, [("Shoulder Bash", "A", shoulderBash), ("Study", "B", study)], "D")
+Ceres = Hero("Ceres", 90, 90, 0, 50, 8, 1, [("Shoulder Bash", "A", shoulderBash), ("Study", "B", study), ("Test", "C", testmove)], "D")
 heroes = [Jade, Kelsey, Cashmere, Ceres]
 
 # enemies
-Slime = Enemy("Slime", 50, 50, 50, {"Goo'd", "Acid Spit"}, "A")
+Slime = Enemy("Slime", 25, 25, 50, {"Goo'd", "Acid Spit"}, "A")
 Goblin = Enemy("Goblin", 75, 75, 5, {"Bash"}, "B")
 Skeleton = Enemy("Skeleton", 100, 100, 10, {"Slice", "Shoot"}, "C")
 enemies = [Slime, Goblin, Skeleton]
